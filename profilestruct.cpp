f1f097de82113982aa7cbe2cefdac2de5210f528
Thuy Nguyen <156226788+thanhthuy280501@users.noreply.github.com>
2025-02-12 23:15:44 -0500

    Update profilestruct.cpp

--- profilestruct.cpp
+++ profilestruct.cpp
@@ -1,13 +1,13 @@
-struct Profile {
+struct Profile {          // Define the profile struct contains fields username, age, and state
     std::string username;
     int age;
     std::string state;
 
-    bool operator==(const Profile& other) const {
+    bool operator==(const Profile& other) const {      //compare profile object
         return username == other.username;
     }
 
-    friend std::ostream& operator<<(std::ostream& os, const Profile& profile) {
+    friend std::ostream& operator<<(std::ostream& os, const Profile& profile) {       //print
         os << "Username: " << profile.username << ", Age: " << profile.age << ", State: " << profile.state;
         return os;
     }
